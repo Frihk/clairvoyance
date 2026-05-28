ba532b0a99513598199fefd45106d53b1e13e0c2
fco1000 <f.18charles@gmail.com>
2026-05-28 13:48:29 +0300

    feat: setup gin router and confirmed it works

--- backend/cmd/server/main.go
+++ backend/cmd/server/main.go
@@ -1,5 +1,14 @@
 package main
 
-func main() {
+import (
+	"clairvoyance/internal/api/router"
+	"log"
+)
 
+func main() {
+	r := router.SetupRouter()
+
+	if err := r.Run(":9000"); err != nil {
+		log.Fatal("Failed to start")
+	}
 }
