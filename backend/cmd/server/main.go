a68949b14b38f040c8b06bccfd66b3a364eda822
ibrsamwel <ibraah568@gmail.com>
2026-05-28 16:22:47 +0300

    updated t handle route function

--- backend/cmd/server/main.go
+++ backend/cmd/server/main.go
@@ -1,5 +1,20 @@
 package main
 
-func main() {
+import (
+	"log"
+	"net/http"
+	"github.com/ibraah007/clairvoyance/backend/internal/db"
+)
 
+func main() {
+	database, err := db.Connect()
+	if err != nil {
+		log.Fatalf("Could not connect to database: %v", err)
+	}
+	defer database.Close()
+
+	mux := setupRoutes(database)
+
+	log.Println("Server starting on :8080...")
+	http.ListenAndServe(":8080", mux)
 }
