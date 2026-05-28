46944a4fd03094afb010e5d5ff9c5f1f61923866
fco1000 <f.18charles@gmail.com>
2026-05-28 14:37:28 +0300

    feat: implement logger initialization and remove unused .keep file

--- backend/cmd/server/main.go
+++ backend/cmd/server/main.go
@@ -2,11 +2,15 @@ package main
 
 import (
 	"clairvoyance/internal/api/router"
+	"clairvoyance/internal/config"
+	"clairvoyance/internal/utils"
 	"log"
 )
 
 func main() {
 	r := router.SetupRouter()
+
+	utils.InitLogger(config.App.AppEnv)
 
 	if err := r.Run(":9000"); err != nil {
 		log.Fatal("Failed to start")
