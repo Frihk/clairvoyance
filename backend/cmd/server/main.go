a225bc828c6e3b0bc58f88ff12b2a88fb025b3a9
fco1000 <f.18charles@gmail.com>
2026-05-28 15:51:49 +0300

    fix: add missing newlines at the end of files and format code for consistency

--- backend/cmd/server/main.go
+++ backend/cmd/server/main.go
@@ -10,9 +10,9 @@ import (
 func main() {
 	config.Load()
 	utils.InitLogger(config.App.AppEnv)
-	
+
 	r := router.SetupRouter()
-	if err := r.Run(":"+config.App.Port); err != nil {
-		log.Fatal("Failed to start:",err)
+	if err := r.Run(":" + config.App.Port); err != nil {
+		log.Fatal("Failed to start:", err)
 	}
 }
--- backend/internal/models/users.go
+++ backend/internal/models/users.go
@@ -8,14 +8,14 @@ import (
 )
 
 type User struct {
-	ID           uuid.UUID `gorm:"type:uuid;primaryKey" json:"id"`
-	WalletAddress        string `gorm:"null" json:"wallet_address"`
-	Email        string    `gorm:"uniqueIndex;not null" json:"email"`
-	PasswordHash string    `gorm:"not null" json:"-"`
-	FullName     string    `gorm:"not null" json:"full_name"`
-	Role     string    `gorm:"default:KES" json:"role"`
-	CreatedAt    time.Time `json:"created_at"`
-	UpdatedAt    time.Time `json:"updated_at"`
+	ID            uuid.UUID `gorm:"type:uuid;primaryKey" json:"id"`
+	WalletAddress string    `gorm:"null" json:"wallet_address"`
+	Email         string    `gorm:"uniqueIndex;not null" json:"email"`
+	PasswordHash  string    `gorm:"not null" json:"-"`
+	FullName      string    `gorm:"not null" json:"full_name"`
+	Role          string    `gorm:"default:KES" json:"role"`
+	CreatedAt     time.Time `json:"created_at"`
+	UpdatedAt     time.Time `json:"updated_at"`
 }
 
 func (u *User) BeforeCreate(tx *gorm.DB) error {
